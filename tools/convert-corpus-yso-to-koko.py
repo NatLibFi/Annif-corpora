926d0dc41d91caa458f9d336e904cac8460c5884
Juho Inkinen <34240031+juhoinkinen@users.noreply.github.com>
2024-09-10 12:20:06 +0300

    Convert all YSO corpus files with one run

--- tools/convert-corpus-yso-to-koko.py
+++ tools/convert-corpus-yso-to-koko.py
@@ -1,9 +1,10 @@
 # Usage:
 #
-# zcat yso-finna-fi-01.tsv.gz | python convert-corpus-yso-to-koko.py | gzip > yso-to-koko-finna-fi-01.tsv.gz
+# python convert-corpus-yso-to-koko.py
 #
 
 import sys
+import gzip
 
 from rdflib import Graph, Literal, Namespace, URIRef
 from rdflib.namespace import RDF, OWL, DCTERMS, SKOS
@@ -30,18 +31,39 @@ for s, p, o in koko.triples((None, URIRef("http://www.w3.org/2004/02/skos/core#e
 assert yso_to_koko['http://www.yso.fi/onto/yso/p6182'] == 'http://www.yso.fi/onto/koko/p5300'  # Lääkärit
 
 
-for line in sys.stdin:
-    if not '\t' in line:
-        continue
+def convert(infile, outfile):
+    for line in infile:
+        if not '\t' in line:
+            continue
 
-    text, *yso_uris = line.split('\t')
+        text, *yso_uris = line.split('\t')
+        if not yso_uris:
+            continue
 
-    koko_uris = [yso_to_koko.get(yso_uri) for yso_uri in yso_uris]
-    # Note: yso uris having not mapped in koko are skipped
-    print(
-        text
-        + '\t'
-        + '\t'.join(
-            [uri for uri in koko_uris if uri is not None]
+        koko_uris = [yso_to_koko.get(yso_uri) for yso_uri in yso_uris]
+
+        # Note: yso uris having not mapped in koko are skipped
+        print(
+            text
+            + '\t'
+            + ' '.join(
+                ['<' + uri + '>' for uri in koko_uris if uri is not None]
+            ),
+            file=outfile
         )
-    )
+
+
+yso_file_names = [
+    'yso-finna-fi-01.tsv.gz',
+    'yso-finna-fi-02.tsv.gz',
+    'yso-finna-fi-03.tsv.gz',
+    'yso-finna-fi-04.tsv.gz',
+]
+
+for ysofname in yso_file_names:
+    kokofilename = ysofname.replace("yso-", "yso-to-koko-")
+    with (
+        gzip.open(ysofname, 'rt') as infile,
+        gzip.open(kokofilename, 'wt') as outfile,
+    ):
+        convert(infile, outfile)
